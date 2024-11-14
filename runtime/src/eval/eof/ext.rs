c0fd2d77af758120d68e688b2dd9827fa6e89cfe
Evgeny Ukhanov <evgeny@aurora.dev>
2024-11-14 01:48:40 +0100

    EIP-7069: ext_call

--- runtime/src/eval/eof/ext.rs
+++ runtime/src/eval/eof/ext.rs
@@ -10,9 +10,10 @@
 //! [EIP-7069](https://eips.ethereum.org/EIPS/eip-7069)
 #![allow(clippy::module_name_repetitions)]
 
-use crate::eval::Control;
-use crate::{ExitError, Handler, Runtime};
-use primitive_types::{H256, U256};
+use crate::eval::{finish_call, Control};
+use crate::{Context, ExitError, Handler, Runtime, Transfer, Vec};
+use evm_core::Capture;
+use primitive_types::{H160, H256, U256};
 
 pub fn return_data_load<H: Handler>(runtime: &mut Runtime, _handler: &mut H) -> Control<H> {
 	require_eof!(runtime);
@@ -31,15 +32,62 @@ pub fn return_data_load<H: Handler>(runtime: &mut Runtime, _handler: &mut H) -> 
 	Control::Continue
 }
 
-pub fn ext_call<H: Handler>(runtime: &mut Runtime, _handler: &mut H) -> Control<H> {
+pub fn ext_call<H: Handler>(runtime: &mut Runtime, handler: &mut H) -> Control<H> {
 	require_eof!(runtime);
 	pop_h256!(runtime, to);
 	// Check if target is left padded with zeroes.
 	if to.0[..12].iter().any(|i| *i != 0) {
 		return Control::Exit(ExitError::InvalidEXTCALLTarget.into());
 	}
+	let to_address = H160::from_slice(&to.0[12..]);
+	runtime.return_data_buffer = Vec::new();
 
-	Control::Continue
+	pop_u256!(runtime, value, in_offset, in_len);
+	// Cast to `usize` after length checking to avoid overflow
+	let in_offset = if in_len == U256::zero() {
+		usize::MAX
+	} else {
+		as_usize_or_fail!(in_offset)
+	};
+	let in_len = as_usize_or_fail!(in_len);
+
+	try_or_fail!(runtime
+		.machine
+		.memory_mut()
+		.resize_offset(in_offset, in_len));
+	let input = if in_len == 0 {
+		Vec::new()
+	} else {
+		runtime.machine.memory().get(in_offset, in_len)
+	};
+	let context = Context {
+		address: to_address,
+		caller: runtime.context.address,
+		eof: runtime.context.eof.clone(),
+		apparent_value: value,
+	};
+	let transfer = Some(Transfer {
+		source: runtime.context.address,
+		target: to_address,
+		value,
+	});
+
+	// TODO: fix gas
+	let gas = None;
+
+	match handler.call(to_address, transfer, input, gas, false, context) {
+		Capture::Exit((reason, return_data)) => {
+			match finish_call(runtime, 0, 0, reason, return_data) {
+				Ok(()) => Control::Continue,
+				Err(e) => Control::Exit(e),
+			}
+		}
+		Capture::Trap(interrupt) => {
+			runtime.return_data_len = 0;
+			runtime.return_data_offset = 0;
+			Control::CallInterrupt(interrupt)
+		}
+	}
 }
 
 pub fn ext_delegate_call<H: Handler>(runtime: &Runtime, _handler: &mut H) -> Control<H> {
